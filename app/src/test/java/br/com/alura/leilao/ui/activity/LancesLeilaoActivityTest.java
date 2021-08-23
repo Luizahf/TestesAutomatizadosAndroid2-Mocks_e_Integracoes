fa88545a22faa3051c8b2395f3ba96cf07a06876
Luíza Freire <luiza.freire@localiza.com>
2021-08-23 19:27:44 -0300

    Começando com os testes do projeto legado

---
+++ app/src/test/java/br/com/alura/leilao/ui/activity/LancesLeilaoActivityTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.leilao.ui.activity;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LancesLeilaoActivityTest {
+
+    @Test
+    public void deve_AtualizarListaLeiloes_quando_ReceberListaLeiloes() {
+        ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(null);
+
+        adapter.atualiza(new ArrayList<Leilao>(Arrays.asList(
+                new Leilao("Console"),
+                new Leilao("Computador")
+        )));
+
+        int quantidadeLeiloesDevolvida = adapter.getItemCount();
+        assertThat(quantidadeLeiloesDevolvida, is(2));
+    }
+}
